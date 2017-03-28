94af4ae75097a9d68606e13a22749a57e10daaf9
David Edwards <knossos@gmail.com>
2017-03-28 16:18:52 +0200

    Amendment to last commit. Include also the change in directory structure.

---
+++ app/src/main/java/de/telecontrol/chargeup/plugin/Constants.java
@@ -0,0 +1,27 @@
+package de.telecontrol.chargeup.plugin;
+
+public class Constants {
+	public static final String PLUGIN_PACKAGE_NAME = "com.welovetv.welovetv";
+	public static final String PLUGIN_INTENT_FILTER = "com.welovetv.plugin.MAIN";
+
+	public static final String PLUGIN_INTENT_COMMAND = "intent-command";
+	public static final String PLUGIN_INTENT_CALLBACK = "intent-callback";
+	public static final String PLUGIN_INTENT_LAUNCH_TARGET = "target";
+
+	public static final String PLUGIN_PAYMENT_OPTIONS = "payment-options";
+	public static final String PLUGIN_PAYMENT_OPTIONS_AMOUNT = "payment-options-amount";
+	public static final String PLUGIN_PAYMENT_OPTIONS_GIVES = "payment-options-gives";
+	public static final String PLUGIN_PAYMENT_OPTIONS_DESCRIPTION = "payment-options-description";
+
+	public static final String PLUGIN_PAYMENT_ALLOWED = "payment-allowed";
+	public static final String PLUGIN_PAYMENT_ALLOWED_BOOLEAN = "payment-allowed-boolean";
+	public static final String PLUGIN_PAYMENT_ALLOWED_MESSAGE = "payment-allowed-message";
+
+	public static final String PLUGIN_PAYMENT_URL = "payment-url";
+	public static final String PLUGIN_PAYMENT_USER = "payment-user";
+
+	public static final String PLUGIN_VERSION = "plugin-version";
+	public static final String PLUGIN_RESPONSE = "plugin-response";
+	public static final String PLUGIN_UNSUPPORTED = "plugin-unsupported";
+	public static final String PLUGIN_OK = "plugin-ok";
+}
---
+++ app/src/main/java/de/telecontrol/chargeup/plugin/PaymentService.java
@@ -0,0 +1,111 @@
+package de.telecontrol.chargeup.plugin;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.os.ResultReceiver;
+import android.support.v4.util.Pair;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public abstract class PaymentService extends IntentService {
+	public static final int API_VERSION = 2;
+
+	public PaymentService() {
+		super("PaymentService");
+	}
+
+	/**
+	 * A list of options for payment. Options include the payment index (in multiples of 5 Euros),
+	 * the title of the payment (short and to the point) and the description of the payment (should
+	 * tell the user exactly what they will get).
+	 * @return A list of {@link PaymentOption}s that will be given as options to the user.
+	 */
+	@NonNull
+	public abstract List<PaymentOption> getPaymentOptions();
+
+	/**
+	 * This must be a properly formatted URL that will be called by our payment service to tell
+	 * your system that money has been paid. Extra parameters will be passed to your service for
+	 * you to verify. Contact a representative to find out what parameters will be passed to you.
+	 * <b>This should be HTTPS!</b> Users will be warned if you provide an insecure URL.
+	 * @return The URL of your payment processing system
+	 */
+	@NonNull
+	public abstract Uri getPaymentUrl();
+
+	/**
+	 * The identifier that uniquely identifies every user in your application. This can be an
+	 * integer ID or an E-mail for example.
+	 * @return The unique user identification string
+	 */
+	@NonNull
+	public abstract String getUserId();
+
+	/**
+	 * Perform some pre-payment testing on your service to ensure that your payment will go through
+	 * without exceptions. You can test that your service is online and able to take the request,
+	 * for example.
+	 * @return A pair containing the absolute true / false of whether your service is reachable,
+	 * and a string message if it isn't.
+	 */
+	@NonNull
+	public abstract Pair<Boolean, String> isPaymentAllowed();
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		final String command = intent.getStringExtra(Constants.PLUGIN_INTENT_COMMAND);
+		final ResultReceiver callback = intent.getParcelableExtra(Constants.PLUGIN_INTENT_CALLBACK);
+
+		if(Constants.PLUGIN_PAYMENT_OPTIONS.equals(command)) {
+			Bundle bundle = new Bundle();
+
+			JSONObject root = new JSONObject();
+			try {
+				JSONArray arr = new JSONArray();
+
+				List<PaymentOption> options = getPaymentOptions();
+				for(PaymentOption option : options) {
+					arr.put(option.asJson());
+				}
+
+				root.put(Constants.PLUGIN_PAYMENT_OPTIONS, arr);
+				root.put(Constants.PLUGIN_PAYMENT_URL, getPaymentUrl());
+				root.put(Constants.PLUGIN_PAYMENT_USER, getUserId());
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			bundle.putString(Constants.PLUGIN_RESPONSE, Constants.PLUGIN_OK);
+			bundle.putString(Constants.PLUGIN_PAYMENT_OPTIONS, root.toString());
+
+			callback.send(1, bundle);
+		} else if(Constants.PLUGIN_PAYMENT_ALLOWED.equals(command)) {
+			Pair<Boolean, String> allowed = isPaymentAllowed();
+
+			Bundle bundle = new Bundle();
+			bundle.putString(Constants.PLUGIN_RESPONSE, Constants.PLUGIN_OK);
+			bundle.putBoolean(Constants.PLUGIN_PAYMENT_ALLOWED_BOOLEAN, allowed.first);
+			bundle.putString(Constants.PLUGIN_PAYMENT_ALLOWED_MESSAGE, allowed.second);
+			callback.send(1, bundle);
+		} else if(Constants.PLUGIN_VERSION.equals(command)) {
+			Bundle bundle = new Bundle();
+			bundle.putString(Constants.PLUGIN_RESPONSE, Constants.PLUGIN_OK);
+			bundle.putInt(Constants.PLUGIN_VERSION, API_VERSION);
+			callback.send(1, bundle);
+		} else {
+			Bundle bundle = new Bundle();
+			bundle.putString(Constants.PLUGIN_RESPONSE, Constants.PLUGIN_UNSUPPORTED);
+			bundle.putString(Constants.PLUGIN_INTENT_COMMAND, command);
+			callback.send(1, bundle);
+		}
+
+		this.stopSelf();
+	}
+}
