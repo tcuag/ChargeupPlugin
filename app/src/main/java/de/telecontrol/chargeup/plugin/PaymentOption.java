9086ef95a7601ec62a5e5d754a2a9e5889cbd0af
David Edwards <knossos@gmail.com>
2018-06-28 13:00:34 +0200

    Update code to use new androidX libraries. BREAKING CHANGE.

--- app/src/main/java/de/telecontrol/chargeup/plugin/PaymentOption.java
+++ app/src/main/java/de/telecontrol/chargeup/plugin/PaymentOption.java
@@ -1,7 +1,7 @@
 package de.telecontrol.chargeup.plugin;
 
-import android.support.annotation.IntRange;
-import android.support.annotation.NonNull;
+import androidx.annotation.IntRange;
+import androidx.annotation.NonNull;
 
 import org.json.JSONException;
 import org.json.JSONObject;
--- app/src/main/java/de/telecontrol/chargeup/plugin/PaymentService.java
+++ app/src/main/java/de/telecontrol/chargeup/plugin/PaymentService.java
@@ -4,9 +4,9 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.support.v4.os.ResultReceiver;
-import android.support.v4.util.Pair;
+import androidx.core.util.Pair;
 
 import org.json.JSONArray;
 import org.json.JSONException;
